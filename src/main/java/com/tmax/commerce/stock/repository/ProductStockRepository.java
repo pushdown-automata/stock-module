15cdc3155f5ba7787ee21f592fefc3820f158d6c
YeEun Lee <yeeun_lee3@tmax.co.kr>
2023-08-24 14:22:55 +0900

    fix: add diamond operator to ProductStockRepository

--- src/main/java/com/tmax/commerce/stock/repository/ProductStockRepository.java
+++ src/main/java/com/tmax/commerce/stock/repository/ProductStockRepository.java
@@ -1,6 +1,7 @@
 package com.tmax.commerce.stock.repository;
 
+import com.tmax.commerce.stock.entity.product.ProductStock;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface ProductStockRepository extends JpaRepository {
+public interface ProductStockRepository extends JpaRepository<ProductStock, Long> {
 }
